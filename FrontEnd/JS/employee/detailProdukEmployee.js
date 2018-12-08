275ca8ecde77a163477f6c74fa6331d020c5fa52
alvin farkhan <alvin.farkhan@gmail.com>
2018-12-09 06:48:07 +0700

    SPA for employee

--- FrontEnd/JS/employee/detailProdukEmployee.js
+++ FrontEnd/JS/employee/detailProdukEmployee.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
     $("#includeHeaderWithSearchBox").load("header/headerWithSearchBoxEmployee.html");
+    $("#includeFormPinjam").load("buatPermintaanPeminjamanEmployee.html");
+    $("#includeFormPinjam").hide();
+    $("#backToDetail").hide();
+});
+
+$("#backToDetail").click(function(){
+    $("#includeFormPinjam").fadeOut();
+    $("#backToDetail").fadeOut();
+});
+
+$("#btnPinjam").click(function(){
+    $("#includeFormPinjam").fadeIn();
+    $("#backToDetail").fadeIn();
 });
--- FrontEnd/JS/employee/homePageEmployee.js
+++ FrontEnd/JS/employee/homePageEmployee.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
     $("#includeHeaderWithSearchBox").load("header/headerWithSearchBoxEmployee.html"); 
+    $("#includeDetailProduk").load("detailProdukEmployee.html"); 
+    $("#includeDetailProduk").hide();
+    $("#closeDetailProduk").hide();
+});
+
+$("#clickDetail").click(function(){
+    $("#includeDetailProduk").fadeIn();
+    $("#closeDetailProduk").fadeIn();
+});
+
+$("#closeDetailProduk").click(function(){
+    $("#includeDetailProduk").fadeOut();
+    $("#closeDetailProduk").fadeOut();
 });
